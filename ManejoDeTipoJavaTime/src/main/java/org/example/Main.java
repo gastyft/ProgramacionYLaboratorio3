2707e9b2d883026679c2fe959bff76a0e9293bee
gastyft <gastyftacla1@hotmail.com>
2024-04-15 04:04:37 -0300

    Dar formato deseado a las fechas y horas

--- ManejoDeTipoJavaTime/src/main/java/org/example/Main.java
+++ ManejoDeTipoJavaTime/src/main/java/org/example/Main.java
@@ -9,15 +9,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
        LocalDate fechaHoy = LocalDate.now();
        LocalTime horaActual = LocalTime.now();
 
         System.out.println("Fecha actual: "+ fechaHoy);
         System.out.println("Hora Actual: "+ horaActual);
         DateTimeFormatter timeFormatter= DateTimeFormatter.ofPattern("HH:mm:ss");
-        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        DateTimeFormatter dateFormatter= DateTimeFormatter.ofPattern("dd/MM/yyyy");
         System.out.println("Fecha Actual con formato: "+fechaHoy.format(dateFormatter));
         System.out.println("Hora Actual con formato: "+horaActual.format(timeFormatter));
+        System.out.println("Ingrese nueva Fecha dd/MM/YYYY");
+        Scanner scanner = new Scanner(System.in);
+       String fechaNueva = scanner.nextLine();
+       fechaHoy = LocalDate.parse(fechaNueva,dateFormatter);
+        System.out.println("Ingrese nueva hora ");
+        String horaNueva = scanner.nextLine();
+        horaActual = LocalTime.parse(horaNueva,timeFormatter);
+        System.out.println(fechaHoy);
+        System.out.println(horaActual);
+
     }
 }
